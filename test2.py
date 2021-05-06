bd8812d5e09a3455a08576b2da96cf4eb66ae0bb
zashii <hideki.ishizashi@gmail.com>
2021-05-06 19:59:00 +0900

    4th

--- test2.py
+++ test2.py
@@ -1,5 +1,10 @@
+# Tkinterのライブラリを取り込む --- (*1)
 import tkinter as tk
-import tkinter.filedialog as tkfd
 
-root = tk.Tk()
-root.withdraw()
+# ウィンドウを作成 --- (*2)
+win = tk.Tk()
+win.title("Hello, World!") # タイトル
+win.geometry("400x300") # サイズ
+
+# ウィンドウを動かす --- (*3)
+win.mainloop()
