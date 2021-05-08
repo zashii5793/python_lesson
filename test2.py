a87bd60a30586be4888ecc9ddde4a2a24c3d029e
zashii <hideki.ishizashi@gmail.com>
2021-05-08 16:32:46 +0900

    6th

--- test2.py
+++ test2.py
@@ -1,31 +1,43 @@
-
 import tkinter as tk
-from tkinter import messagebox as mbox
 
-# ウィンドウを作成 --- (*1)
+# ボタンを押したときの処理 --- (*1)
+def calc_bmi():
+    # BMIを計算
+    h = float(textHeight.get()) / 100
+    w = float(textWeight.get())
+    bmi = w / h ** 2
+    rw = h ** 2 * 22
+    per = int(w / rw * 100) - 100
+    # 結果をラベルに表示
+    s = "肥満 {0}% (bmi={1})".format(per, bmi)
+    labelResult['text'] = s
+
+# ウィンドウを作成 --- (*2)
 win = tk.Tk()
-win.geometry("500x250") # サイズを指定
+win.title("肥満判定")
+win.geometry("500x250")
 
-# 部品を作成 --- (*2)
-# ラベルを作成
-label = tk.Label(win, text='名前は?')
-# 作成した部品をウィンドウ上に配置
-label.pack()
+# 部品を作成 --- (*3)
+labelHeight = tk.Label(win, text=u'身長(cm):')
+labelHeight.pack()
 
-# テキストボックスを作成
-text = tk.Entry(win)
-text.pack()
-text.insert(tk.END, 'クジラ') # 初期値を指定
+textHeight = tk.Entry(win)
+textHeight.insert(tk.END, '160')
+textHeight.pack()
 
-# OKボタンを押した時 --- (*3)
-def ok_click():
-    # テキストボックスの内容を得る
-    s = text.get()
-    # ダイアログを表示
-    mbox.showinfo('挨拶', s + 'さん、こんにちは!')
-# ボタンを作成 --- (*4)
-okButton = tk.Button(win, text='OK', command=ok_click)
-okButton.pack()
+labelWeight = tk.Label(win, text=u'体重(kg):')
+labelWeight.pack()
+
+textWeight = tk.Entry(win)
+textWeight.insert(tk.END, '70')
+textWeight.pack()
+
+labelResult = tk.Label(win, text=u'---')
+labelResult.pack()
+
+calcButton = tk.Button(win, text=u'計算')
+calcButton["command"] = calc_bmi
+calcButton.pack()
 
 # ウィンドウを動かす
 win.mainloop()
