4c9ac248d3947639c2d16d9b35594d956426c464
zashii <hideki.ishizashi@gmail.com>
2021-05-28 07:58:54 +0900

    25th

--- filecopy-2.py
+++ filecopy-2.py
@@ -1,7 +1,15 @@
 import shutil
+import openpyxl
 
 try:
     shutil.copytree('./before','./after')
 except FileExistsError as e:
     print('すでにafterフォルダが存在しています')
-    
+
+if 0 > x:
+
+    else:
+
+print('a' in 'abc')
+
+
