3c13c1a931487ac6cb0a46e101d923f1b01353cb
zashii <hideki.ishizashi@gmail.com>
2021-06-05 10:44:13 +0900

    33th

--- Excel_Python.py
+++ Excel_Python.py
@@ -1,4 +1,12 @@
 import openpyxl
+openpyxl.path.abspath()
+
+https://techacademy.jp/magazine/18986
+
+# base.pyのあるディレクトリの絶対パスを取得
+current_dir = pathlib.Path(__file__).resolve().parent
+# モジュールのあるパスを追加
+sys.path.append( str(current_dir) + '/../' )
 
 #Macでは、optionキーを押しながら、¥キーを押す。(Mac mini 等で Windows用のキーボードを接続している場合は Alt キー)
 
