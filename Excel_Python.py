4e45be6deead68113a243d723541a25f32ab3fbe
zashii <hideki.ishizashi@gmail.com>
2021-06-06 15:41:23 +0900

    32th

--- Excel_Python.py
+++ Excel_Python.py
@@ -1,13 +1,14 @@
-import openpyxl
-openpyxl.path.abspath()
 
-https://techacademy.jp/magazine/18986
+#export PYTHONPATH="/Users/user/opt/anaconda3/lib/python3.8/site-packages/openpyxl:$PYTHONPATH"
+
+import openpyxl
+import os
+
+#openpyxl.path.abspath()
+#/Users/user/opt/anaconda3/lib/python3.8/site-packages/openpyxl
+#https://techacademy.jp/magazine/18986
 
 # base.pyのあるディレクトリの絶対パスを取得
-current_dir = pathlib.Path(__file__).resolve().parent
-# モジュールのあるパスを追加
-sys.path.append( str(current_dir) + '/../' )
-
 #Macでは、optionキーを押しながら、¥キーを押す。(Mac mini 等で Windows用のキーボードを接続している場合は Alt キー)
 
 wb = openpyxl.load_workbook(".\list.xlsx")
